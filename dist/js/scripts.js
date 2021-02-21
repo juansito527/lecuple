5a3c6e7a2fcd24e9443025c96768ba3d223c9c65
juansito527 <patousa1292@gmail.com>
2021-02-21 11:05:59 -0500

    primera parte frontend

---
+++ dist/js/scripts.js
@@ -0,0 +1,10 @@
+/* para deplegar el nav  */
+(function($) {
+    "use strict";
+
+    // Toggle the side navigation
+    $("#sidebarToggle").on("click", function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+    });
+})(jQuery);
